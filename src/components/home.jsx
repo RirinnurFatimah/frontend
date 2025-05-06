10eee85150f686e25fd2817d465a07fca1d670e3
PanjiSetyadi <setyadipanjisetya@gmail.com>
2025-05-06 14:15:21 +0700

    first

---
+++ src/components/home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const home = () => {
+  return (
+    <div className="flex items-center justify-center h-screen text-3xl font-bold text-center bg-amber-200 text-blue-400">
+      Hello World
+    </div>
+  )
+}
+
+export default home
