5b1e98044560353819c68c8bd369910a4dfac4f3
RirinnurFatimah <154777598+RirinnurFatimah@users.noreply.github.com>
2025-05-29 10:57:36 +0700

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Landing from './components/landing';
 import Navbar from './components/navbar';
 import Home from './components/home';
 import Scanner from './views/ScannerView';
